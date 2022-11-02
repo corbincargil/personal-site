1a1ec8a0f2fd3ed49307b021c19b072ad866a407
corbincargil <corbin.cargil@gmail.com>
2022-11-02 16:08:46 -0500

    added vanta background to Hero.js and Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyledContact } from "./styles/Contact.styled";
 import ContactForm from "./ContactForm";
 import linkedInIcon from "../images/linkedin-48.png";
 import githubIcon from "../images/github-60.png";
 
+import * as THREE from "three";
+import CELLS from "vanta/dist/vanta.cells.min";
+
 export default function Contact() {
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        CELLS({
+          THREE: THREE,
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          color1: 0x584f98,
+          color2: 0x40450,
+          size: 4.6,
+          speed: 1.7,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <StyledContact>
+    <StyledContact ref={myRef}>
       <ContactForm />
       <div id="connect">
         <h2>Connect with me</h2>
--- src/components/styles/Hero.styled.js
+++ src/components/styles/Hero.styled.js
@@ -2,27 +2,26 @@ import styled from "styled-components";
 import hero from "../../images/hero.jpg";
 
 export const StyledHero = styled.div`
-  display: flex;
-  flex-direction: column;
   height: 100vh;
-  #hero-bg {
-    margin-top: -235px;
-    position: absolute;
-    background-image: url(${hero});
-    background-size: cover;
+  #animation {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
     width: 100%;
-    height: 100vh;
   }
   p {
     position: absolute;
     bottom: 60px;
     left: 10px;
   }
+  :nth-child(2) {
+    justify-self: center;
+  }
 `;
 
 export const Logo = styled.div`
-  background-color: #ffffff3e;
-  margin-top: 300px;
+  position: relative;
+  top: 50vh;
   display: flex;
   justify-content: flex-end;
   img {
