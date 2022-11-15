784f5158c054fa9d23e387600e36d95c1ce2c12a
corbincargil <corbin.cargil@gmail.com>
2022-11-15 16:29:28 -0600

    added form functionality

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import { StyledForm } from "./styles/Contact.styled";
 
 export default function ContactForm() {
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
   const [form, setForm] = useState({
-    email: "",
     name: "",
+    email: "",
+    subject: "",
     message: "",
   });
 
@@ -17,60 +26,84 @@ export default function ContactForm() {
   }
 
   function submitForm(e) {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() =>
+        alert(
+          `Thank you, ${form.name}! Your contact request has been submitted.`
+        )
+      )
+      .catch((error) => alert(error));
+
     e.preventDefault();
-    alert(`Thank you, ${form.name}! Your contact request has been submitted.`);
   }
 
   return (
-    <StyledForm id="contact" method="POST" name="contact">
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        placeholder="Your name"
-        required
-        minLength={5}
-        maxLength={30}
-        onInput={onUserInput}
-        value={form.name}
-      />
-      <label htmlFor="email">Your email:</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="example@gmail.com"
-        required
-        minLength={6}
-        maxLength={40}
-        onInput={onUserInput}
-        value={form.email}
-      />
-      <label htmlFor="subject">Subject:</label>
-      <input
-        type="text"
-        name="subject"
-        id="subject"
-        placeholder="Subject"
-        required
-        minLength={3}
-        maxLength={30}
-        onInput={onUserInput}
-        value={form.subject}
-      />
-      <label htmlFor="message">Message:</label>
-      <textarea
-        name="message"
-        id="message"
-        rows={5}
-        placeholder="Type your message here..."
-        required
-        minLength={6}
-        maxLength={150}
-        onInput={onUserInput}
-        value={form.message}
-      />
+    <StyledForm
+      id="contact"
+      // onSubmit={submitForm}
+      name="contact"
+      data-netlify="true"
+    >
+      <label htmlFor="name">
+        Name:
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your name"
+          required
+          minLength={5}
+          maxLength={30}
+          // onInput={onUserInput}
+          // value={form.name}
+        />
+      </label>
+      <label htmlFor="email">
+        Your email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="example@gmail.com"
+          required
+          minLength={6}
+          maxLength={40}
+          // onInput={onUserInput}
+          // value={form.email}
+        />
+      </label>
+      <label htmlFor="subject">
+        Subject:
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Subject"
+          required
+          minLength={3}
+          maxLength={30}
+          // onInput={onUserInput}
+          // value={form.subject}
+        />
+      </label>
+      <label htmlFor="message">
+        Message:
+        <textarea
+          name="message"
+          id="message"
+          rows={5}
+          placeholder="Type your message here..."
+          required
+          minLength={6}
+          maxLength={150}
+          // onInput={onUserInput}
+          // value={form.message}
+        />
+      </label>
       <button id="submit" type="submit">
         Send
       </button>
