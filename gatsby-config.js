e566f876ced7af1d02584fb5ab7a111947d940c8
corbincargil <corbin.cargil@gmail.com>
2022-11-24 20:27:28 -0600

    added gatsby-plugin-netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-smoothscroll",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-filesystem",
       options: {
