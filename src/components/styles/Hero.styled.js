a1ba27dbd4355ae4f0e0b9c7f1774f8207ba51c8
corbincargil <corbin.cargil@gmail.com>
2022-11-29 15:52:54 -0600

    updated hero styling

--- src/components/styles/Hero.styled.js
+++ src/components/styles/Hero.styled.js
@@ -8,8 +8,16 @@ export const StyledHero = styled.div`
     height: 100%;
     width: 100%;
   }
+
   #animation .gatsby-image-wrapper {
-    width: clamp(250px, 80%, 1000px);
-    margin-left: 20%;
+    width: 60%;
+    margin-left: 40%;
+  }
+
+  @media screen and (max-width: 1199.98px) {
+    #animation .gatsby-image-wrapper {
+      width: clamp(250px, 75%, 1000px);
+      margin-left: 25%;
+    }
   }
 `;
