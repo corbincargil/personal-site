a4ae9ab832186644eb16bc76130189cee8e74fb2
corbincargil <corbin.cargil@gmail.com>
2023-07-04 11:26:41 -0500

    add skills and rearrange categories. update skills styling.

--- src/components/styles/Skills.styled.js
+++ src/components/styles/Skills.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const StyledSkills = styled.div`
-  /* background-color: #06074a; */
   background-color: white;
   color: #06074a;
   margin-top: 2rem;
@@ -22,21 +21,14 @@ export const StyledSkills = styled.div`
     height: fit-content;
     display: grid;
     gap: 1rem;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    justify-items: center;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(1, 1fr);
     border-bottom: 1px solid #e6af48;
   }
-  .learned :first-child,
-  .learned :nth-child(5) {
-    grid-row-start: 1;
-    grid-row-end: span 2;
-  }
+
   .line {
-    grid-row-start: 1;
-    grid-row-end: span 2;
     border-left: 1px solid #e6af48;
-    height: 80%;
+    height: 100%;
   }
 
   .learning {
@@ -46,21 +38,16 @@ export const StyledSkills = styled.div`
   .skill-list {
     list-style: none;
     padding: 16px;
-    /* background-color: #e6af48; */
-    /* background-color: white; */
     background-color: #06074a22;
     border: 2px solid black;
     border-radius: 10px;
     transition: 0.1s;
     width: 75%;
   }
-  /* .skill-list:hover {
-    box-shadow: rgb(0, 0, 0) 1px 1px 1px 0px inset,
-      rgba(18, 18, 18, 0.5) -1px -1px 1px 1px inset;
-    transition: 0.1s;
-  } */
+
   .skill-list ul {
     margin: 0;
+    margin-left: 10%;
     padding: 0;
   }
   .skill {
@@ -79,24 +66,18 @@ export const StyledSkills = styled.div`
     width: 36px;
   }
 
-  @media screen and (max-width: 991.98px) {
+  @media screen and (max-width: 1150px) {
     .learning {
       width: 50%;
+    }
+    .learned {
+      grid-template-columns: repeat(2, 1fr);
     }
   }
 
   @media screen and (max-width: 767.98px) {
     .line {
       display: none;
-    }
-    .learned {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    .learned :nth-child(5) {
-      grid-row-start: 1;
-      grid-row-end: span 2;
-      grid-column-start: 2;
-      grid-column-end: span 1;
     }
     .learning {
       width: 90%;
@@ -110,19 +91,15 @@ export const StyledSkills = styled.div`
     }
   }
 
-  @media screen and (max-width: 575.98px) {
-    .skill-list ul {
-      margin-left: 30%;
-    }
+  @media screen and (max-width: 600px) {
     .learned {
       grid-template-columns: repeat(1, 1fr);
     }
-    .learned :nth-child(5) {
-      grid-column: span 1;
-      grid-row: span 1;
-    }
     .learning {
       align-self: center;
+    }
+    .learned {
+      justify-items: center;
     }
     #current-learning {
       margin: auto;
