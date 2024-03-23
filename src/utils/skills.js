a708183acd1b8bf6dfa29bdd758396c02e855c7f
Corbin <corbo@Corbins-MBP-2.attlocal.net>
2024-03-23 11:19:37 -0500

    update skills

---
+++ src/utils/skills.js
@@ -0,0 +1,199 @@
+//skill icons
+import reactIcon from "../images/skill-icons/icons8-react-native-48.png";
+import htmlIcon from "../images/skill-icons/icons8-html-5-48.png";
+import cssIcon from "../images/skill-icons/icons8-css3-48.png";
+import jsIcon from "../images/skill-icons/icons8-javascript-48.png";
+import firebaseIcon from "../images/skill-icons/icons8-google-firebase-console-48.png";
+import gatsbyIcon from "../images/skill-icons/icons8-gatsbyjs-48.png";
+import gitIcon from "../images/skill-icons/icons8-git-48.png";
+import npmIcon from "../images/skill-icons/icons8-npm-48.png";
+import webpackIcon from "../images/skill-icons/icons8-webpack-48.png";
+import jestIcon from "../images/skill-icons/jest.png";
+import figmaIcon from "../images/skill-icons/icons8-figma-48.png";
+import nodeIcon from "../images/skill-icons/icons8-nodejs-48.png";
+import styledComponentsIcon from "../images/skill-icons/styled-components-icon.png";
+import expressIcon from "../images/skill-icons/icons8-express-js-48.png";
+import mongoDBIcon from "../images/skill-icons/icons8-mongodb-48.png";
+import graphQLIcon from "../images/skill-icons/graphql-48.png";
+import nextIcon from "../images/skill-icons/icons8-next.js-48.png";
+import tsIcon from "../images/skill-icons/icons8-typescript-48.png";
+import testingLibraryIcon from "../images/skill-icons/octopus-128x128.png";
+import dockerIcon from "../images/skill-icons/icons8-docker-48.png";
+import prismaIcon from "../images/skill-icons/icons8-prisma-orm-48.png";
+import prostgreIcon from "../images/skill-icons/icons8-postgresql-48.png";
+import tailwindIcon from "../images/skill-icons/icons8-tailwindcss-48.png";
+import viteIcon from "../images/skill-icons/icons8-vite-48.png";
+import restApiIcon from "../images/skill-icons/icons8-rest-api-48.png";
+import goIcon from "../images/skill-icons/icons8-golang-48.png";
+import vimIcon from "../images/skill-icons/icons8-vim-48.png";
+
+export const skills = [
+  {
+    name: "Typescript",
+    icon: tsIcon,
+    category: "front-end",
+    id: 18,
+  },
+  {
+    name: "Next.js",
+    icon: nextIcon,
+    category: "industry-tools",
+    id: 17,
+  },
+  {
+    name: "React",
+    icon: reactIcon,
+    category: "front-end",
+    id: 1,
+  },
+  {
+    name: "Javascript (ES6)",
+    icon: jsIcon,
+    category: "front-end",
+    id: 4,
+  },
+  {
+    name: "Vite",
+    icon: viteIcon,
+    category: "industry-tools",
+    id: 25,
+  },
+  {
+    name: "React Testing Library",
+    icon: testingLibraryIcon,
+    category: "industry-tools",
+    id: 19,
+  },
+  {
+    name: "Docker",
+    icon: dockerIcon,
+    category: "industry-tools",
+    id: 23,
+  },
+  {
+    name: "Gatsby",
+    icon: gatsbyIcon,
+    category: "industry-tools",
+    id: 6,
+  },
+  {
+    name: "Git/Github/Gitlab",
+    icon: gitIcon,
+    category: "industry-tools",
+    id: 7,
+  },
+  {
+    name: "Node.js",
+    icon: nodeIcon,
+    category: "back-end",
+    id: 13,
+  },
+  {
+    name: "Webpack",
+    icon: webpackIcon,
+    category: "industry-tools",
+    id: 9,
+  },
+  {
+    name: "Jest",
+    icon: jestIcon,
+    category: "industry-tools",
+    id: 10,
+  },
+  {
+    name: "Figma",
+    icon: figmaIcon,
+    category: "other",
+    id: 11,
+  },
+  {
+    name: "Styled Components",
+    icon: styledComponentsIcon,
+    category: "front-end",
+    id: 12,
+  },
+  {
+    name: "NPM",
+    icon: npmIcon,
+    category: "other",
+    id: 8,
+  },
+  {
+    name: "Express.js",
+    icon: expressIcon,
+    category: "back-end",
+    id: 14,
+  },
+  {
+    name: "GraphQL",
+    icon: graphQLIcon,
+    category: "other",
+    id: 16,
+  },
+  {
+    name: "PostgreSQL",
+    icon: prostgreIcon,
+    category: "back-end",
+    id: 21,
+  },
+  {
+    name: "Prisma",
+    icon: prismaIcon,
+    category: "back-end",
+    id: 23,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwindIcon,
+    category: "front-end",
+    id: 22,
+  },
+  {
+    name: "Mongo DB",
+    icon: mongoDBIcon,
+    category: "back-end",
+    id: 15,
+  },
+  {
+    name: "HTML5",
+    icon: htmlIcon,
+    category: "front-end",
+    id: 2,
+  },
+  {
+    name: "CSS3",
+    icon: cssIcon,
+    category: "front-end",
+    id: 3,
+  },
+  {
+    name: "Rest API",
+    icon: restApiIcon,
+    category: "back-end",
+    id: 24,
+  },
+  {
+    name: "Firebase",
+    icon: firebaseIcon,
+    category: "back-end",
+    id: 5,
+  },
+  {
+    name: "Go",
+    icon: goIcon,
+    category: "current-learning",
+    id: 26,
+  },
+  {
+    name: "Vim",
+    icon: vimIcon,
+    category: "current-learning",
+    id: 27,
+  },
+  //   {
+  //     name: "",
+  //     icon: ,
+  //     category: "",
+  //     id: 28,
+  //   },
+];
