c01c9b12e7430067ad9b61a83709a5c6d4c64e37
Corbin <corbin.cargil@gmail.com>
2024-09-19 18:56:57 -0500

    add netlify cli

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 const { getStore } = require("@netlify/blobs");
 
 exports.handler = async function (event, context) {
-  const store = getStore("site-visits");
+  let store;
+  if (process.env.NETLIFY) {
+    // Running on Netlify
+    store = getStore("site-visits");
+  } else {
+    // Running locally
+    store = getStore({
+      name: "site-visits",
+      siteID: process.env.SITE_ID,
+      token: process.env.NETLIFY_AUTH_TOKEN,
+    });
+  }
+
   const now = Date.now();
   const lastVisitTime = await store.get("lastVisitTime");
-  const rateLimit = 600000; // 10 minutes in milliseconds
+  const rateLimit = 60000; // 1 minutes in milliseconds
 
   if (lastVisitTime && now - lastVisitTime < rateLimit) {
+    console.log("Rate limit exceeded, no email sent");
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Email skipped due to rate limiting" }),
@@ -34,11 +48,13 @@ exports.handler = async function (event, context) {
 
   try {
     await sgMail.send(msg);
+    console.log("Email sent successfully");
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Email sent successfully" }),
     };
   } catch (error) {
+    console.error("Failed to send email", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Failed to send email" }),
